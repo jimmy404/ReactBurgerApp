a74f7d3b95b699126ee20dfbf4cebf3484289a99
Jimmy200 <bruno_pier@hotmail.com>
2021-03-15 18:05:05 -0300

    Adds Burger into BurgerBuilder compoent

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Burger from '../../components/Burger/Burger';
 
 import Auxiliary from '../../hoc/Auxiliary';
 
@@ -6,7 +7,7 @@ class BurgerBuilder extends Component {
   render () {
     return (
       <Auxiliary>
-        <div>Burger</div>
+        <Burger />
         <div>Build Controls</div>
       </Auxiliary>
     );
