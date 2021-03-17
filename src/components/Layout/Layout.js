b1122307cc1015def3466ad25da71c08eeb4fd48
Jimmy200 <bruno_pier@hotmail.com>
2021-03-17 12:40:36 -0300

    Adds responsive adjustments for mobile version

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 import classes from './Layout.css';
@@ -7,6 +8,7 @@ import classes from './Layout.css';
 const layout = (props) => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>
       {props.children}
     </main>
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+import classes from './SideDrawer.css'
+
+const sideDrawer = (props) => {
+  // TODO > conditional classes to show or not SD
+  return (
+    <div className={classes.SideDrawer}>
+      <div className={classes.Logo}>
+        <Logo />
+      </div>
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
+  );
+};
+
+export default sideDrawer;
