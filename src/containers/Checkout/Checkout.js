7950dbc764703b571d89feef0ede6d18d22cc1aa
Jimmy200 <bruno_pier@hotmail.com>
2021-03-22 11:35:20 -0300

    Pass ingredients through query parameters

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -13,6 +13,15 @@ class Checkout extends Component {
     }
   }
 
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for ( let params of query.entries()) {
+      ingredients[params[0]] = +params[1];
+    }
+    this.setState({ingredients: ingredients});
+  }
+
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
   };
