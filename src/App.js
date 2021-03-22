91cb267feeb53bad40da91ae6249d5abec0ea203
Jimmy200 <bruno_pier@hotmail.com>
2021-03-22 13:36:55 -0300

    Adds an Order&Orders file and styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 import Layout from './hoc/Layout/Layout';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './Order.css';
+
+const order = (props) => (
+  <div className={classes.Order}>
+    <p>Ingredients: Salad (1)</p>
+    <p>Price: <stron>USD 5.45</stron></p>
+  </div>
+);
+
+export default order;
