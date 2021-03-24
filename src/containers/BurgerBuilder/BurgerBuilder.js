a7512e36043cb072bfbff1eaa45e44fc5ca853f0
Jimmy200 <bruno_pier@hotmail.com>
2021-03-24 15:23:11 -0300

    Adds redux, store & reducer

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -29,13 +29,13 @@ class BurgerBuilder extends Component {
   }
 
   componentDidMount () {
-    axios.get('https://burgerbuilder-39c39-default-rtdb.firebaseio.com/ingredients.json')
-      .then(response => {
-        this.setState({ingredients: response.data});
-      })
-      .catch(error => {
-        this.setState({error: true});
-      })
+    // axios.get('https://burgerbuilder-39c39-default-rtdb.firebaseio.com/ingredients.json')
+    //   .then(response => {
+    //     this.setState({ingredients: response.data});
+    //   })
+    //   .catch(error => {
+    //     this.setState({error: true});
+    //   })
   }
 
   updatePurchaseState (ingredients) {
