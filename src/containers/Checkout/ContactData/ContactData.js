9e222a6991d94975aa2ce40886c0252407f94235
Jimmy200 <bruno_pier@hotmail.com>
2021-03-24 16:38:39 -0300

    Refactored code to include redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,5 +1,7 @@
-import axios from '../../../axios-orders';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import axios from '../../../axios-orders';
+
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
@@ -101,7 +103,7 @@ class ContactData extends Component {
     formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
   }
   const order = {
-    ingredients: this.props.ingredients,
+    ingredients: this.props.ings,
     price: this.props.price,
     orderData: formData
   };
@@ -198,4 +200,11 @@ class ContactData extends Component {
   }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice
+  };
+};
+
+export default connect(mapStateToProps)(ContactData);
