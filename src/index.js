e9d4e6741de74075deed5c3eb53320c5ba708e66
Jimmy200 <bruno_pier@hotmail.com>
2021-03-25 16:08:05 -0300

    Adds redux thunk config

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './App';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
@@ -11,7 +12,11 @@ import './index.css';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(burgerBuilderReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
 
 const app = (
   <Provider store={store}>
