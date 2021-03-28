02f48a75827dffae2ac1abe9cc47302b7f78b11f
Jimmy200 <bruno_pier@hotmail.com>
2021-03-28 14:08:08 -0300

    Improves UX with Redirect

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -94,7 +94,6 @@ class ContactData extends Component {
       },
     },
     formIsValid: false,
-    loading: false
   }
 
   orderHandler = (event) => {
@@ -185,7 +184,7 @@ class ContactData extends Component {
         disabled={!this.state.formIsValid}
       >Order</Button>
     </form>);
-    if (this.state.loading) {
+    if (this.props.loading) {
       <Spinner/>
     }
     return(
@@ -200,12 +199,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
   return {
     ings: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    loading: state.loading
   };
 };
 
 const mapDispatchToProps = dispatch => {
-  onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+  return {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+  }
 };
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
