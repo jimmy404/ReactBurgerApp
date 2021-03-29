91940117b22cd27e15b2fe9f4a3c2f6c71b6ffcf
Jimmy200 <bruno_pier@hotmail.com>
2021-03-29 13:51:17 -0300

    Adds sign in code

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,15 +21,19 @@ export const authFail = (error) => {
   };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart());
     const authData = {
       email: email,
       password: password,
       returnSecureToken: true
+    };
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA3TZbQX7P1yTSxmM64FVM4V5niTDBz4r0';
+    if (!isSignup) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA3TZbQX7P1yTSxmM64FVM4V5niTDBz4r0';
     }
-    axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA3TZbQX7P1yTSxmM64FVM4V5niTDBz4r0', authData)
+    axios.post(url, authData)
       .then(response => {
         console.log(response);
         dispatch(authSuccess(response.data));
