7237ef482f490abf8c1807e5683bed74c5f8a905
Jimmy200 <bruno_pier@hotmail.com>
2021-03-30 19:33:40 -0300

    Fixes a couple of issues in login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Route, Switch, withRouter } from 'react-router-dom';
 
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
@@ -7,9 +8,11 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Layout from './hoc/Layout/Layout';
-
-
+import * as actions from './store/actions/index';
 class App extends Component {
+  componentDidMount () {
+    this.props.onTryAutoSignup();
+  }
   render() {
     return (
       <div>
@@ -27,4 +30,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState)
+  }
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
